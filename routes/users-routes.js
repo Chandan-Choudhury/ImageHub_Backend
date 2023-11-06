decc5b6112ca1456cff09320dba29b28ec5245de
CHANDAN CHOUDHURY <62041378+Chandan-Choudhury@users.noreply.github.com>
2023-11-06 11:00:09 +0530

    Suspend SIGNUP Route
    - Temporarily suspend SIGNUP Route

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -9,15 +9,15 @@ const userRateLimiter = require("../middlewares/userRateLimit");
 
 const router = express.Router();
 
-router.post(
-  "/signup",
-  [
-    check("name").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
-  usersControllers.signup
-);
+// router.post(
+//   "/signup",
+//   [
+//     check("name").not().isEmpty(),
+//     check("email").normalizeEmail().isEmail(),
+//     check("password").isLength({ min: 6 }),
+//   ],
+//   usersControllers.signup
+// );
 
 router.post("/login", usersControllers.login);
 
